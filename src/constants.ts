f271151f8b8171dac8cc9ba240e539df04c8fc49
Dr hrushikesh Gosai <drhrushiomfsurgeon@gmail.com>
2025-11-21 12:00:32 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,2 @@
-
 export const INITIAL_LABELS = ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'];
-
-export const DEFAULT_ANNOTATION_COLOR = 'rgba(150, 150, 150, 0.7)'; // A neutral grey
+export const DEFAULT_ANNOTATION_COLOR = 'rgba(150, 150, 150, 0.7)';
